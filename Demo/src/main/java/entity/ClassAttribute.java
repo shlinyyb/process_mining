538abe5e2c4bbecebc80f1f114025ade6c642efa
shlinyyb <shlinyyb@gmail.com>
2020-03-23 15:21:40 +0800

    lombok

--- Demo/src/main/java/entity/ClassAttribute.java
+++ Demo/src/main/java/entity/ClassAttribute.java
@@ -1,24 +1,25 @@
 package entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @Description:
  * @Author: Kobe
  * @Date: 2020/3/20 12:56
  */
-@Data
+@Getter
+@Setter
 public class ClassAttribute {
     private String authority;
     private String returnType;
     private String attributeName;
 
-    public ClassAttribute(String authority, String returnType, String attributeName){
+    public ClassAttribute(String authority, String returnType, String attributeName) {
         this.authority = authority;
         this.returnType = returnType;
         this.attributeName = attributeName;
     }
-
 
 
 }
